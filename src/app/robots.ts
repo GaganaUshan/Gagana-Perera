278060792b3521390089110237cbab96eaade37c
GaganaUshan <gaganaushan16@gmail.com>
2026-03-25 11:05:14 +0530

    feat: add sitemap and robots.txt for SEO

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://gagana-perera.vercel.app/sitemap.xml',
+  };
+}
